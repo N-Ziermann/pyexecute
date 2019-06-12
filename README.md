2c14cbe7c0797053c5620be53df9300f0afff2f6
Niklas Ziermann <n-ziermann@protonmail.com>
2019-06-12 16:55:49 +0200

    Improved linux compatibility

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # pyopen
+
+**Version 1.0.0**
+
+## Installation
+
+Put the "*pyopen.py*" file into your project folder or your *PYTHONPATH* and import it like this:
+
+```python
+import pyopen
+```
+
+## Dependencies
+
+#### Python 3.6 (or higher) with the following modules:
+
+- os
+- multiprocessing
+
+Both of these are part of the default python install and therefore don't need to be installed separately.
+
+## Usage
+
+```python
+pyopen.file(path):	 # opens a file in its default editor
+    pass
+
+pyopen.program(path):# opens a program (for linux path is just a prorgams name e.g "obs")
+    pass
+```
+
+## Contributors
+
+Niklas Ziermann
+
+## Copyright & License
+
+**© Niklas Ziermann** 
+
+**MIT License**
+
--- pyopen.py
+++ pyopen.py
@@ -1,14 +1,21 @@
-import os
+import os, multiprocessing
 
-def file(path): # open a file in it's default editor
-    if os.name == "nt": # windows
-        os.startfile(content)
-    else:               # linux
+
+def file(path):             ### open a file in it's default editor
+    if os.name == "nt":     ## windows
+        os.startfile(path)
+    else:                   ## linux
         os.system("xdg-open " + path)
 
 
-def program(path):  # open a program
-    if os.name == "nt": # windows
-        os.startfile(content)
-    else:               # linux
-        os.system(path)
+def programLinux(path):
+    os.system(path)
+
+
+def program(path):          ### open a program
+    if os.name == "nt":     ## windows
+        os.startfile(path)
+    else:                   ## linux
+        # opens in seperate process to prevent python from waiting for program to close
+        p = multiprocessing.Process(target=programLinux, args=(path,))
+        p.start()
